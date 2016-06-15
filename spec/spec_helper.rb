c30d0a4a485cfbeaf236fe4ab9302f29ee6689c7
Mike Grigorov <mikesg@abv.bg>
2016-06-15 14:04:43 +0300

    Modify Gem to work with rails 4+ without support for create_table time comments

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,11 +8,11 @@ require 'activerecord-column_metadata'
 
 ActiveRecord::Base.logger = Logger.new(File.dirname(__FILE__) + '/debug.log')
 ActiveRecord::Base.configurations = YAML::load_file(File.dirname(__FILE__) + '/database.yml')
-ActiveRecord::Base.establish_connection(ENV['DB'] || 'postgresql')
+ActiveRecord::Base.establish_connection(ENV['DB'] || :postgresql)
 
-ActiveRecord::Base.silence do
-  ActiveRecord::Migration.verbose = false
-end
+#ActiveRecord::Base.silence do
+#  ActiveRecord::Migration.verbose = false
+#end
 
 RSpec.configure do |config|
 
